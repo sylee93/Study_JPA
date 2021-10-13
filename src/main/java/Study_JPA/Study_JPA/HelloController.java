d4815a1e3128054074be2197e5ea5a704c2afe81
master <sylee050864@gmail.com>
2021-10-14 00:10:43 +0900

    view 환경설정

---
+++ src/main/java/Study_JPA/Study_JPA/HelloController.java
@@ -0,0 +1,15 @@
+package Study_JPA.Study_JPA;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+}
