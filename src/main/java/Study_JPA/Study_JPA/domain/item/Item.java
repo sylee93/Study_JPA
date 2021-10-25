38b0c85f9a0adf49030b36f6f6965dbb2639dd7b
master <sylee050864@gmail.com>
2021-10-26 00:24:59 +0900

    주문 기능 테스트

--- src/main/java/Study_JPA/Study_JPA/domain/item/Item.java
+++ src/main/java/Study_JPA/Study_JPA/domain/item/Item.java
@@ -30,7 +30,6 @@ public class Item {
 
     /**
      * stock 증가
-     * @param quantity
      */
     public void addStock(int quantity){
         this.stockQuantity += quantity;
@@ -38,10 +37,9 @@ public class Item {
 
     /**
      * stock 감소
-     * @param quantity
      */
     public void removeStock(int quantity){
-        int restStock = this.stockQuantity = quantity;
+        int restStock = this.stockQuantity - quantity;
         if(restStock < 0){
             throw new NotEnoughStockException("need more stock");
         }
