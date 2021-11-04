d2e039ec15e3df9107a14a31b5e259eb18fc37a1
master <sylee050864@gmail.com>
2021-11-04 23:40:22 +0900

    상품 주문

---
+++ src/main/java/Study_JPA/Study_JPA/controller/OrderController.java
@@ -0,0 +1,46 @@
+package Study_JPA.Study_JPA.controller;
+
+import Study_JPA.Study_JPA.domain.Member;
+import Study_JPA.Study_JPA.domain.item.Item;
+import Study_JPA.Study_JPA.service.ItemService;
+import Study_JPA.Study_JPA.service.MemberService;
+import Study_JPA.Study_JPA.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model){
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members",members);
+        model.addAttribute("items",items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count);
+        return  "redirect:/orders";
+    }
+
+}
