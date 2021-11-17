c2e00a6e9b944e1a6d3b79abe5e3a193d5af9b40
master <sylee050864@gmail.com>
2021-11-17 22:50:06 +0900

    지연 로딩과 조회 성능 최적화 간단한 주문 조회 V2 : 엔티티를 DTO 변환 N+1 문제 발생

--- src/main/java/Study_JPA/Study_JPA/api/OrderSimpleApiController.java
+++ src/main/java/Study_JPA/Study_JPA/api/OrderSimpleApiController.java
@@ -1,14 +1,20 @@
 package Study_JPA.Study_JPA.api;
 
 
+import Study_JPA.Study_JPA.domain.Address;
 import Study_JPA.Study_JPA.domain.Order;
+import Study_JPA.Study_JPA.domain.OrderStatus;
 import Study_JPA.Study_JPA.repository.OrderRepository;
 import Study_JPA.Study_JPA.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * x To one
@@ -32,5 +38,32 @@ public class OrderSimpleApiController {
         }
 
         return  all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // Lazy 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // LAZY 초기화
+        }
     }
 }
